1955d6730e2b03e6aa95277fa4bca87ae683b6ef
Brian Hogan <brianhogan@napcs.com>
2013-06-06 16:09:18 -0500

    updated to include slate.js file for more control over slate config

--- .slate.js
+++ .slate.js
@@ -2,6 +2,8 @@ var focusTerminal = slate.operation("focus", { "app" : "iTerm" });
 var focusBrowser = slate.operation("focus", {"app" : "Google Chrome"});
 
 var lastFocusedApp = "";
+
+// Toggle between iTerm and the last used app
 slate.bind("1:ctrl;cmd", function(windowObject) {
   var currentAppName = windowObject.app().name();
   if (currentAppName !== "iTerm") {
