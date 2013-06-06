219c5dbdeed551b40180c4508b950082da1e435b
Brian Hogan <brianhogan@napcs.com>
2013-06-06 16:07:33 -0500

    updated to include slate.js file for more control over slate config

---
+++ .slate.js
@@ -0,0 +1,24 @@
+var focusTerminal = slate.operation("focus", { "app" : "iTerm" });
+var focusBrowser = slate.operation("focus", {"app" : "Google Chrome"});
+
+var lastFocusedApp = "";
+slate.bind("1:ctrl;cmd", function(windowObject) {
+  var currentAppName = windowObject.app().name();
+  if (currentAppName !== "iTerm") {
+    lastFocusedApp = currentAppName; // store the app name in lastFocusedApp if it is not MacVim
+    focusTerminal.run(); // focus iTerm
+  } else {
+    // If the current app is iTerm, focus the lastFocusedApp
+    slate.operation("focus", { "app" : lastFocusedApp }).run();
+  }
+});
+
+// Toggle between iTerm and Chrome
+slate.bind("2:ctrl;cmd", function(windowObject) {
+  var currentAppName = windowObject.app().name();
+  if (currentAppName !== "iTerm") {
+    focusTerminal.run(); // focus iTerm
+  } else {
+    focusBrowser.run(); // focus Chrome
+  }
+});
